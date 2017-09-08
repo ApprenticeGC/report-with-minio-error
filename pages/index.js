3187592d84666a9da9c0b3b4005c0ae5253e04e4
ApprenticeGC <apprentice@giantcroissant.com>
2017-09-08 17:35:39 +0800

    A near blank Next.js project to show errors occurred using with minio.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// Once the requrie of Minio is removed, the error will be shown
+// const Minio = require('minio')
+
+// These modules were not found:
+
+// * fs in ./node_modules/mkdirp/index.js, ./node_modules/minio/dist/main/minio.js
+
+// To install it, you can run: npm install --save fs
+
+export default () => (
+    <div>
+        <h1>Hello Next.js</h1>
+    </div>
+)
